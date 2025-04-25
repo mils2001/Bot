5da34111a89dc1bd62f9dd0c8d0fa6300902f2b6
Chrismiles <mileschris466@gmail.com>
2025-04-26 01:16:24 +0300

    update on vite

--- Bot/vite.config.js
+++ Bot/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // 👈 Make Vite output to "build" instead of "dist"
-  },
-});
+})
+
 
